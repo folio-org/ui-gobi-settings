dd5d72906427b8df69c88c12b19caf78634841b7
Yury Saukou <yury_saukou@epam.com>
2025-07-31 00:20:16 +0400

    fix another test

--- src/settings/components/FieldFromFieldName/FieldFromFieldName.test.js
+++ src/settings/components/FieldFromFieldName/FieldFromFieldName.test.js
@@ -39,10 +39,8 @@ describe('FieldFromFieldName', () => {
   it('should change \'Path\' when gobi field name was changed', async () => {
     renderFieldFromFieldName();
 
-    await act(async () => {
-      await userEvent.click(screen.getByText('stripes-components.selection.controlLabel'));
-      await userEvent.click(screen.getByText(defaultProps.dataOptions[1].label));
-    });
+    await act(async () => userEvent.click(screen.getByText('stripes-components.selection.controlLabel')));
+    await act(async () => userEvent.click(screen.getByText(defaultProps.dataOptions[1].label)));
 
     expect(defaultProps.change).toHaveBeenCalled();
   });
